5415a7261917a61669547e03d562728836e19d1d
momtazrasel <momtazrasel8@gmail.com>
2023-10-29 13:45:30 +0600

    Project is updated

--- src/Static_Variable.java
+++ src/Static_Variable.java
@@ -1,9 +1,21 @@
 public class Static_Variable {
-    public static final String Department = "Department";
+    private static final String DepartmentName = "Department Name is CSE";
     private static double Salary = 50000;
+
+    public static double salary;
+
+    private static String name;
+    public void setSalary(double empSalary, String empName){
+        salary = empSalary;
+        name = empName;
+    }
 
     public static void main(String[] args) {
 //        Salary = 5000;
-        System.out.println(Department + "Salary Average is: "+Salary);
+        System.out.println(DepartmentName);
+        Static_Variable obj = new Static_Variable();
+        obj.setSalary(15000,  "Momtaz Rasel");
+        System.out.println("Employee Name is: " +name);
+        System.out.println("Employee Salary is: " +salary);
     }
 }
