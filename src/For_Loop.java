ec638b865d9f8d9d5f7cde178d50d453f90e4e8a
momtazrasel <momtazrasel8@gmail.com>
2023-11-04 00:23:07 +0600

    Switch Case Updated

--- src/For_Loop.java
+++ src/For_Loop.java
@@ -20,10 +20,24 @@ public class For_Loop {
         }
     }
 
+    public void numArrayPrint(){
+
+        int num[] = {10,20,30,40,50};
+
+        for (int x =0; x<num.length; x++){
+            System.out.println("Array name is: "+num[x]+",");
+        }
+
+        for (int y : num){
+            System.out.println("Array name is: "+y+",");
+        }
+    }
+
     public static void main(String[] args) {
         For_Loop obj = new For_Loop();
         obj.numberPrint();
         obj.namePrint();
         obj.numPrint();
+        obj.numArrayPrint();
     }
 }
