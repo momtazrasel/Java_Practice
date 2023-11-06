49e556d7d714309b4934925c352c689c90b347e2
momtazrasel <momtazrasel8@gmail.com>
2023-11-06 11:42:24 +0600

    While Loop

--- src/While_Loop.java
+++ src/While_Loop.java
@@ -6,6 +6,12 @@ public class While_Loop {
             System.out.println("X values is: "+x);
             x++;
     }
+        int numbers[] = {10,20,30,40,50};
+        int y = 0;
+        while (y<5){
+            System.out.println("This is y values "+numbers[y]);
+            y++;
+        }
     }
 
     public static void main(String[] args) {
