11a7eda8fb50383573718eafbe84753df07db7b0
momtazrasel <momtazrasel8@gmail.com>
2023-11-09 21:29:46 +0600

    Objective,Class, Constructor

---
+++ src/Class_In_Java.java
@@ -0,0 +1,20 @@
+public class Class_In_Java {
+    // this is class name is Class_In_Java
+    //Instance variable
+    int x;
+    String xyz;
+    public void methodOne(){
+        //This is the method
+        //local variable
+        int x;
+    }
+
+    public void methodTwo(){
+        //This is the method
+    }
+
+    public Class_In_Java(String name){
+        //this is constructor
+        //this constructor contains one parameter
+    }
+}
