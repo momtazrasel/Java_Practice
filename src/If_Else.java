d43a15e74c06ee1a14c4920c032ec1bbe38f011a
momtazrasel <momtazrasel8@gmail.com>
2023-11-16 22:50:30 +0600

    Projectis updated

--- src/If_Else.java
+++ src/If_Else.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class If_Else {
 
     public void ifElseStatement(){
         int x = 50;
-//        int y = 50;
+        int y = 50;
 
         if (x<40){
             System.out.println("This is If statement");
         }else{
             System.out.println("This is else statement");
+        }
+
+        if(x==y){
+            System.out.println("X and Y's values are equal");
+        }
+        else {
+            System.out.println("Not Equal");
+        }
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please Enter the Number");
+        int num = reader.nextInt();
+        if (num%2 ==0){
+            System.out.println("This is Even Number: "+num);
+        }
+        else {
+            System.out.println("This is odd number: "+num);
         }
     }
 
