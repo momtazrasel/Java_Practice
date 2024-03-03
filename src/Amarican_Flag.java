1fa0d456c3209a3b7046a541ed77b329a81dc735
momtazrasel <momtazrasel8@gmail.com>
2024-03-04 00:24:42 +0600

    American Flag

--- src/Amarican_Flag.java
+++ src/Amarican_Flag.java
@@ -2,8 +2,14 @@ public class Amarican_Flag {
     public void flag(){
 
         for (int i=0; i<=5; i++){
-            System.out.println("*");
-            System.out.println("=");
+            System.out.println("* * * * * *  ==================================");
+            if (i ==4){
+                break;
+            }
+            System.out.println(" * * * * *   ==================================");
+        }
+        for (int y =0; y<=5;y++){
+            System.out.println("===============================================");
         }
 
     }
