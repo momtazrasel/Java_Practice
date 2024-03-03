171eac0d7de0b68accaa8fc851f520f955f01686
momtazrasel <momtazrasel8@gmail.com>
2024-03-03 23:55:22 +0600

    American Flag

---
+++ src/Amarican_Flag.java
@@ -0,0 +1,11 @@
+public class Amarican_Flag {
+    public void flag(){
+
+    }
+    public static void main(String[] args) {
+        Amarican_Flag test = new Amarican_Flag();
+        test.flag();
+    }
+
+}
+
