cbd8a8aba81bfd9f7d097c4298dc7207f919fbe0
momtazrasel <momtazrasel8@gmail.com>
2024-03-06 00:12:49 +0600

    Fibonacci

--- src/FibonacciSerice.java
+++ src/FibonacciSerice.java
@@ -3,7 +3,13 @@ public class FibonacciSerice {
         int x= 0;
         int y= 1;
         int z = x+y;
-        System.out.println(z);
+        System.out.println(x+" "+y);
+        for (int i =2; i<10; i++){
+            z = x+y;
+            System.out.println(z);
+            x= y;
+            y=z;
+        }
     }
     public static void main(String[] args) {
         FibonacciSerice test = new FibonacciSerice();
