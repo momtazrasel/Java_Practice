eb1afe4b42e192301e1b8c18952173af617d1fb6
momtazrasel <momtazrasel8@gmail.com>
2024-03-08 12:42:04 +0600

    Palindrom Updated

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,8 +1,14 @@
 public class Palindrome {
     public void checkPalindrome(){
-        int sum = 0;
+        int sum = 0, tem,r, n=454;
+        while (n>0){
+            r = n%10;
+            System.out.println(r);
+        }
     }
     public static void main(String[] args) {
 
+        Palindrome test = new Palindrome();
+        test.checkPalindrome();
     }
 }
