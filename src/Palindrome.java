35872a6faabf5ccd8c3f2cb5d053a8b1a038897c
momtazrasel <momtazrasel8@gmail.com>
2024-03-09 23:14:16 +0600

    Palindrome is updated

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,9 +1,16 @@
 public class Palindrome {
     public void checkPalindrome(){
         int sum = 0, tem,r, n=454;
+        tem = n;
         while (n>0){
             r = n%10;
-            System.out.println(r);
+            sum = (sum*10)+r;
+            n = n/10;
+        }
+        if(tem == sum){
+            System.out.println("Number is palindrome");
+        }else {
+            System.out.println("Number is not palindrome");
         }
     }
     public static void main(String[] args) {
