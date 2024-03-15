5f49eb6bd9efe9df40e901047f473bf03f67ed78
momtazrasel <momtazrasel8@gmail.com>
2024-03-15 17:22:33 +0600

    Random Number

--- src/RandomNumberGenerate.java
+++ src/RandomNumberGenerate.java
@@ -6,7 +6,8 @@ public class RandomNumberGenerate {
         double a = Math.random()*(max-min+1)+min;
         System.out.println(a);
 
-        int b = (int) Math.random()*(max-min+1)+min;
+        int b = (int) (Math.random()*(max-min+1)+min);
+        System.out.println(b);
 
     }
 
