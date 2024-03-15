b51bc04a5917cc3a0d85b103bb4aa31296ebfacc
momtazrasel <momtazrasel8@gmail.com>
2024-03-15 17:19:29 +0600

    Random Number

---
+++ src/RandomNumberGenerate.java
@@ -0,0 +1,17 @@
+public class RandomNumberGenerate {
+    public void generateRandomNumber(){
+        int max = 200;
+        int min = 400;
+
+        double a = Math.random()*(max-min+1)+min;
+        System.out.println(a);
+
+        int b = (int) Math.random()*(max-min+1)+min;
+
+    }
+
+    public static void main(String[] args) {
+        RandomNumberGenerate random = new RandomNumberGenerate();
+        random.generateRandomNumber();
+    }
+}
