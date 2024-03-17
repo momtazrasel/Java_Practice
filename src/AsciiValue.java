e78a0cd0f6c820f95c48de0fbae4027693a86cd2
momtazrasel <momtazrasel8@gmail.com>
2024-03-17 10:31:28 +0600

    ASCII Value

--- src/AsciiValue.java
+++ src/AsciiValue.java
@@ -1,2 +1,20 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class AsciiValue {
+
+    public void printASCIIValue(){
+        char value1 = 'A';
+        char value2 = 'B';
+        System.out.println("Value1 is: "+value1 + " Value2 is : "+value2);
+
+        int asciiVale1 = value1;
+        int asciiValue2 = value2;
+
+        System.out.println("ASCII Value 1 is: "+asciiVale1+" ASCII Value 2 is :"+asciiValue2);
+    }
+
+    public static void main(String[] args) {
+        AsciiValue obj = new AsciiValue();
+        obj.printASCIIValue();
+    }
 }
