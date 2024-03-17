9183a936766991cc7672d3f0982e060ee14c4d25
momtazrasel <momtazrasel8@gmail.com>
2024-03-17 10:51:47 +0600

    Reverse using while loop

---
+++ src/ReverseNumber.java
@@ -0,0 +1,18 @@
+public class ReverseNumber {
+    int x = 1234;
+    int sum = 0, rev=0, number, rem;
+    public void reverseUsingWhile(){
+        while (x>0){
+             rem = x%10;
+             rev = rev*10+rem;
+             x = x/10;
+        }
+        System.out.println("reverse number is "+rev);
+    }
+
+    public static void main(String[] args) {
+
+        ReverseNumber obj = new ReverseNumber();
+        obj.reverseUsingWhile();
+    }
+}
