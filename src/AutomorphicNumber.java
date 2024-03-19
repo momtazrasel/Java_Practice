cbf5e56c2576dcb4b65e2140b0bced750a79a9a3
momtazrasel <momtazrasel8@gmail.com>
2024-03-20 00:01:24 +0600

    Automorphic Number

--- src/AutomorphicNumber.java
+++ src/AutomorphicNumber.java
@@ -1,2 +1,13 @@
 public class AutomorphicNumber {
+    public void checkAutomorphicNumber(int num){
+        int squre = num*num;
+        while (num>0){
+            if(num%10 != squre%10)
+                return;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
