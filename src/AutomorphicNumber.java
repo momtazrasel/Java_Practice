e452c5aba3204b7e8bd99e7ad137f59fe665b718
momtazrasel <momtazrasel8@gmail.com>
2024-03-21 11:58:25 +0600

    Automorphic Number

--- src/AutomorphicNumber.java
+++ src/AutomorphicNumber.java
@@ -10,6 +10,18 @@ public class AutomorphicNumber {
         int count = 0;
         int square = num*num;
         System.out.println(square);
+        int temp = num;
+        while (temp>0){
+            count++;
+            temp = temp/10;
+
+        }
+        int lastDigit = (int)(square%(Math.pow(10,count)));
+        if (num == lastDigit){
+            System.out.println(num + " Number is automorphic");
+        }else {
+            System.out.println(num + " Number is not automorphic");
+        }
     }
 
     public static void main(String[] args) {
