db6e3756488b7d6db73498070849bb1e9a91cdc6
momtazrasel <momtazrasel8@gmail.com>
2024-03-21 11:32:59 +0600

    Automorphic Number

--- src/AutomorphicNumber.java
+++ src/AutomorphicNumber.java
@@ -1,13 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class AutomorphicNumber {
-    public void checkAutomorphicNumber(int num){
-        int squre = num*num;
-        while (num>0){
-            if(num%10 != squre%10)
-                return;
-        }
+    public void checkAutomorphicNumber(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = in.nextInt();
+        System.out.println(num);
+        int count = 0;
+        int square = num*num;
+        System.out.println(square);
     }
 
     public static void main(String[] args) {
-
+        AutomorphicNumber obj = new AutomorphicNumber();
+        obj.checkAutomorphicNumber();
     }
 }
