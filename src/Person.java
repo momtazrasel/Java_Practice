7e913b5d862d0bc9ac094e777c0ba11f4f4a2ac6
momtazrasel <momtazrasel8@gmail.com>
2024-05-10 00:04:54 +0600

    Person Class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,22 @@
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("momtaz",26);
+        System.out.println("Name: " +person.getName());
+        System.out.println("Name: " +person.getAge());
+    }
 }
