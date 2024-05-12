8fe3d1cb90ccdf4b6fb5799b8a41d4ed2ba12f0f
momtazrasel <momtazrasel8@gmail.com>
2024-05-13 00:01:52 +0600

    Rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,23 @@
 public class Rectangle {
-    int height;
-    int weidth;
+    private int height;
+    private int width;
+
+    public void areOfRectangle(int height, int width){
+        this.height = height;
+        this.width = width;
+        int area = height*width;
+        System.out.println(area);
+    }
+    public void perimeterOfRectangle(int height, int width){
+        this.height = height;
+        this.width = width;
+        int perimeter = 2*(height+width);
+        System.out.println(perimeter);
+    }
+
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle();
+        obj.areOfRectangle(10,20);
+        obj.perimeterOfRectangle(10,20);
+    }
 }
