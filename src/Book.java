0bddf994842cc7c474b6aae0f257baabdce05653
momtazrasel <momtazrasel8@gmail.com>
2024-05-16 23:42:33 +0600

    Book class is updated

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,43 @@
 public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+
+    public Book(String title, String author, String ISBN){
+        this.title = title;
+        this.author = author;
+        this.author = author;
+
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(){
+        this.title = title;
+    }
+    public String getAuthor(){
+        return title;
+    }
+    public void setAuthor(){
+        this.title = title;
+    }
+    public String getISBN(){
+        return title;
+    }
+    public void setISBN(){
+        this.title = title;
+    }
+    public void addBook(){
+
+    }
+
+    public void removeBook(){
+        
+    }
+
+    public static void main(String[] args) {
+        Book obj = new Book("Himu","Humayon Ahmed" , "Bangla bajar");
+        System.out.println(obj.getTitle());
+    }
 }
