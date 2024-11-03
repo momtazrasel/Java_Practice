c7faf815d35a7f69bced5becc92a3f56bcef7b1d
momtazrasel <momtazrasel8@gmail.com>
2024-11-03 22:05:46 +0600

    sort program

---
+++ src/SortArrayWithoutMethod.java
@@ -0,0 +1,6 @@
+public class SortArrayWithoutMethod {
+    public static void main(String[] args) {
+        int[] arr = new int[] {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65};
+        System.out.println("Array element s after sorting");
+    }
+}
