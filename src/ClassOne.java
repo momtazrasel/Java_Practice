e5a71c87a498ce9b9c71af1a791ee4ce5efbd259
momtazrasel <momtazrasel8@gmail.com>
2024-12-07 00:10:52 +0600

    New Class

--- src/ClassOne.java
+++ src/ClassOne.java
@@ -1,8 +1,21 @@
 public class ClassOne {
+    public void sumTwoDigit(int x, int y){
+        int z = x+y;
+        System.out.println("Total Sum:- "+z);
+    }
+
     public static void main(String[] args) {
         for (int i=0; i<=10; i++){
             System.out.println("print number: "+i);
+            if (i==8){
+                break;
+            }
+            else {
+                System.out.println("Successfully done");
+            }
         }
         System.out.println("Hello World");
+        ClassOne obj = new ClassOne();
+        obj.sumTwoDigit(12,23);
     }
 }
---
+++ src/ClassTwo.java
@@ -0,0 +1,8 @@
+public class ClassTwo {
+    public ClassOne obj;
+    public ClassTwo(){
+        this.obj = new ClassOne();
+    }
+
+
+}
